91ceb1ae5270adfb7111b018b603ac1ffba44654
sigallev <speledleviatan@gmail.com>
2022-01-11 13:08:38 +0200

    return and save unconverted

--- BuildPhIPSeqLibrary/construct_nucleotide_sequences.py
+++ BuildPhIPSeqLibrary/construct_nucleotide_sequences.py
@@ -73,23 +73,23 @@ def barcode_sequences(oligo_sequences):
             lambda nuc: get_barcode_from_nuc_seq(nuc, start_location, barcode_length))
         start_location += barcode_length
     existing_barcodes = read_barcoded_nucleotide_files()
+    cols = existing_barcodes.columns
     uncoded_oligos = []
     # Add barcoded oligos one at a time
     for oligo_id, oligo_row in oligo_sequences.iterrows():
-        if all([oligo_row[f'barcode_{i}'] not in existing_barcodes[f'barcode_{i}'] for i in
-                range(len(BARCODE_NUC_LENGTHS))]):
-            existing_barcodes = existing_barcodes.append(oligo_row)
+        if False: #all([oligo_row[f'barcode_{i}'] not in existing_barcodes[f'barcode_{i}'] for i in
+                #range(len(BARCODE_NUC_LENGTHS))]):
+            existing_barcodes = existing_barcodes.append(oligo_row[cols])
         else:
             # Try to create a new barcode section
             new_oligo_row = create_new_nuc_sequence(oligo_row, existing_barcodes)
             if new_oligo_row['nuc_sequence'] is not None:
-                existing_barcodes = existing_barcodes.append(new_oligo_row)
+                existing_barcodes = existing_barcodes.append(new_oligo_row[cols])
             else:
-                uncoded_oligos.append(new_oligo_row)
+                uncoded_oligos.append(new_oligo_row.copy())
     # Save barcodes
     existing_barcodes.to_csv(BARCODED_NUC_FILE)
-    # TODO: check the type of uncoded oligos and save it
-    return existing_barcodes
+    return oligo_sequences, pd.concat(uncoded_oligos, axis=1).T
 
 
 def update_unconverted_oligos_file(unconverted_oligos):
@@ -103,6 +103,7 @@ def aa_to_nuc(oligos_aa_sequences):
     if len(unconverted_oligos) > 0:
         update_unconverted_oligos_file(unconverted_oligos)
     oligos_aa_sequences = oligos_aa_sequences[oligos_aa_sequences['nuc_sequence'].notnull()].copy()
-    ret = barcode_sequences(oligos_aa_sequences)
-    print("here")
+    ret, unconverted_oligos = barcode_sequences(oligos_aa_sequences)
+    if len(unconverted_oligos) > 0:
+        update_unconverted_oligos_file(unconverted_oligos)
     return ret
