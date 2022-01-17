cd3cc7c4378b8946bd846205e4bbe2e1f058e197
sigall <speledleviatan@gmail.com>
2022-01-17 07:33:47 +0200

    print

--- BuildPhIPSeqLibrary/main.py
+++ BuildPhIPSeqLibrary/main.py
@@ -1,5 +1,6 @@
 import argparse
 import os.path
+import time
 
 from BuildPhIPSeqLibrary.config import OUTPUT_DIR, BARCODED_NUC_FILE, ORDER_FILE
 from BuildPhIPSeqLibrary.construct_nucleotide_sequences import aa_to_nuc
@@ -25,24 +26,27 @@ for filename in os.listdir('{OUTPUT_DIR}'):
 
     files = get_input_files()
     for filename in files:
-        print(f"Working on file {filename}")
-        print(f"{filename}: reading file")
+        print(f"Working on file {filename}", time.ctime())
+        print(f"{filename}: reading file", time.ctime())
         seq_id_to_sequences = read_file(filename)
-        print(f"{filename}: Got {len(seq_id_to_sequences)} sequences")
-        print(f"{filename}: Identifying new sequences")
+        print(f"{filename}: Got {len(seq_id_to_sequences)} sequences", time.ctime())
+        print(f"{filename}: Identifying new sequences", time.ctime())
         seq_id_to_sequences = add_sequences_to_files_list(seq_id_to_sequences, filename)
-        print(f"{filename}: Got {len(seq_id_to_sequences)} new sequences")
-        print(f"{filename}: Converting sequences to oligos")
+        print(f"{filename}: Got {len(seq_id_to_sequences)} new sequences", time.ctime())
+        print(f"{filename}: Converting sequences to oligos", time.ctime())
         all_oligos_aa_sequences, new_oligos_aa_sequences = split_and_map_new_sequences(seq_id_to_sequences)
         print(f"{filename}: "
               f"Converted {len(new_oligos_aa_sequences)} new oligos "
-              f"overall {len(all_oligos_aa_sequences)} oligos")
-        print(f"{filename}: Barcoding oligos")
+              f"overall {len(all_oligos_aa_sequences)} oligos", time.ctime())
+        print(f"{filename}: Barcoding oligos", time.ctime())
         oligo_barcoded_sequences = aa_to_nuc(new_oligos_aa_sequences)
         print(
             f"{filename}: Finished barcoding oligos. "
-            f"Current number of oligos is {len(oligo_barcoded_sequences)}")
-    print(f"Finished creating sequences. Find them in {BARCODED_NUC_FILE}")
-    print(f"Converting all sequences to order")
-    transfer_to_order(oligo_barcoded_sequences)
-    print(f"Find file of order in {ORDER_FILE}")
+            f"Current number of oligos is {len(oligo_barcoded_sequences)}", time.ctime())
+    if len(files) > 0:
+        print(f"Finished creating sequences. Find them in {BARCODED_NUC_FILE}", time.ctime())
+        print(f"Converting all sequences to order", time.ctime())
+        transfer_to_order(oligo_barcoded_sequences)
+        print(f"Find file of order in {ORDER_FILE}", time.ctime())
+    else:
+        print("No new files")
