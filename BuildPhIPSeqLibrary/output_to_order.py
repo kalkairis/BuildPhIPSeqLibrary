57f806978583abb107c24e9d7aa477107afd54cc
sigall <speledleviatan@gmail.com>
2022-01-19 14:54:48 +0200

    refactor

--- BuildPhIPSeqLibrary/output_to_order.py
+++ BuildPhIPSeqLibrary/output_to_order.py
@@ -1,7 +1,7 @@
-from BuildPhIPSeqLibrary.config import PREFIX, SUFFIX ORDER_FILE
+from BuildPhIPSeqLibrary.config import PREFIX, SUFFIX, ORDER_FILE
 
 
 def transfer_to_order(oligo_barcoded_sequences):
-    nuc_sequences = '\n'.join((PREFIX_PROMOTER + oligo_barcoded_sequences['nuc_sequence'] + SUFFIX_PROMOTER).values)
+    nuc_sequences = '\n'.join((PREFIX + oligo_barcoded_sequences['nuc_sequence'] + SUFFIX).values)
     with open(ORDER_FILE, 'w') as file:
         file.write(nuc_sequences)
